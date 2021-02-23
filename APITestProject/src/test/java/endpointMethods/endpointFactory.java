6fe4d6ef15aae7012e0f3dc027b8027a7e1ffcc3
Ashwin Nayak <19385660+ashwinnayak@users.noreply.github.com>
2021-02-23 15:25:41 +0530

    Add files via upload

---
+++ APITestProject/src/test/java/endpointMethods/endpointFactory.java
@@ -0,0 +1,14 @@
+package endpointMethods;
+
+public class endpointFactory {
+
+    public static final String Post ="posts";
+    public static final String SinglePost ="posts/{id}";
+    public static final String SingleComments ="comments/{id}";
+    public static final String Comments="comments";
+    public static final String SingleUsers ="users/{id}";
+    public static final String Users="users";
+
+    public static final String TestURL="https://jpjupiter-rt-stage13.campaign.adobe.com/nl/jsp/soaprouter.jsp";
+
+}
